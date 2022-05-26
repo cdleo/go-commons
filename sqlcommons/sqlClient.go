a950825b90c93282a02a174f08cd7f1852397ff1
ext_christleo <ext_christleo@falabella.cl>
2022-05-26 11:18:26 -0300

    Moving defines to go-sqldb project

--- sqlcommons/sqlClient.go
+++ sqlcommons/sqlClient.go
@@ -5,24 +5,6 @@ import (
 	"database/sql"
 )
 
-//Drivers
-const (
-	Oracle_DriverName     = "oracle"
-	OracleTNS_DriverName  = "oracle-tns"
-	Postgresql_DriverName = "postgres"
-	SQLite3_DriverName    = "sqlite3"
-	MockDB_DriverName     = "mockdb"
-)
-
-type SQLEngineAdapter interface {
-	Open() (*sql.DB, error)
-	ErrorHandler(err error) error
-}
-
-type SQLSyntaxTranslator interface {
-	Translate(query string) string
-}
-
 type SQLClient interface {
 	Open() error
 	Close()
